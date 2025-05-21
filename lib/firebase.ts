cbf928f83b1ad15eb010d6b62b09ee85d5af8c16
janginseok <589second@gmail.com>
2025-05-22 00:43:35 +0900

    fix: firebase import/export, lint/type errors

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,21 +3,18 @@
 import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 import { getAuth } from "firebase/auth"
-import { getAnalytics } from "firebase/analytics"
 
 const firebaseConfig = {
   apiKey: "AIzaSyCwuEBcOw-owbx4f8W8tOvYNf6yBcl3_cI",
   authDomain: "lost-japan.firebaseapp.com",
   projectId: "lost-japan",
-  storageBucket: "lost-japan.firebasestorage.app",
+  storageBucket: "lost-japan.appspot.com", // .app(X) → .app**spot**.com(맞음)
   messagingSenderId: "34657974674",
-  appId: "1:34657974674:web:cde944c058c740509f294b",
-  measurementId: "G-ZWGLKB5WTS",
+  appId: "1:34657974674:web:cde944c058c740509f294b"
 }
 
 const app = initializeApp(firebaseConfig)
-const analytics = getAnalytics(app)
 const db = getFirestore(app)
 const auth = getAuth(app)
 
-export { app, analytics, db, auth }
+export { app, db, auth }
